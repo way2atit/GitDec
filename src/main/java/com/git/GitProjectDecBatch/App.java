6f028d1403852baaf101afe9e054a66f65186b67
way2atit <way2atit@gmail.com>
2022-03-30 11:45:32 +0530

    First Commited file

---
+++ src/main/java/com/git/GitProjectDecBatch/App.java
@@ -0,0 +1,13 @@
+package com.git.GitProjectDecBatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/git/LoginTest.java
@@ -0,0 +1,10 @@
+package com.git;
+
+public class LoginTest {
+	int i =10;
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
