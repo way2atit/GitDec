c0c373d45a2c2a2a9f05bb8fd06f3d6e47d81a32
way2atit <97036773+way2atit@users.noreply.github.com>
2022-03-31 11:57:07 +0530

    serevr test2

--- src/test/java/com/git/Test2.java
+++ src/test/java/com/git/Test2.java
@@ -8,5 +8,24 @@ public class Test2 {
 
 	int num1 = 10;
 	int num2 = 100;
+	
+	public static void m1() // user 
+	{
+		
+	}
+	
+	public static int m2()
+	{
+		return 10;
+	}
+	
+	
+	public static String m3() 
+	{
+		return "abc";
+	}
+
+	
+	
 
 }
